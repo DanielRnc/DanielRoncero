4b25a26b3c6f67a957118567c7395062afbc902b
DanielRnc <130442827+DanielRnc@users.noreply.github.com>
2023-05-25 13:53:04 +0200

    fun3

---
+++ ej3.java
@@ -0,0 +1,39 @@
+package examen;
+
+import java.util.Scanner;
+
+public class ej3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Escribe por consola un numero");
+		
+		int num1 = sc.nextInt();
+		
+		
+		do {
+			--num1;
+			System.out.println(num1);
+		}while (num1 != 1);
+		
+		
+		System.out.println("Escribe por consola un numero");
+		
+		int num2 = sc.nextInt();
+		do {
+			
+			num2 = %2;
+			
+			System.out.println(num2);
+		}while (num2 !=1);
+			
+		
+		
+		
+		
+		
+	}
+
+	}
+
